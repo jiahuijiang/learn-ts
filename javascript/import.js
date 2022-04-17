d58657efe3dedb31f7768c81973766401eb14d4e
Jiahui Jiang <jiahui@makenotion.com>
2022-04-16 21:31:20 -0400

    module

---
+++ javascript/import.js
@@ -0,0 +1,6 @@
+import theDefault, { nonDefaultFunction2, nonDefaultFunction3 } from "./module.js";
+
+const input = "input";
+theDefault(input);
+nonDefaultFunction2(input);
+nonDefaultFunction3(input);
---
+++ javascript/module.js
@@ -0,0 +1,11 @@
+export default function(input) {
+    console.log("default function " + input);
+}
+
+export function nonDefaultFunction2(input) {
+    console.log("non-default function 2 " + input);
+}
+
+export function nonDefaultFunction3(input) {
+    console.log("non-default function 3 " + input);
+}
